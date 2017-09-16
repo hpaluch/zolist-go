236dae9975507444f4e9634ec8eb2e5aace1800f
Henryk Paluch <hpaluch@seznam.cz>
2017-09-16 12:00:05 +0200

    Added status codes

--- gofmt.sh
+++ gofmt.sh
@@ -4,14 +4,17 @@
 gofmt=/opt/gae/google-cloud-sdk/platform/google_appengine/goroot-1.8/bin/gofmt
 set -x
 $gofmt -d *.go 
+set +x
 echo -n "Reformat source [y/N]?"
 read ans
 case "$ans" in
   y*)
     $gofmt -w *.go
+    echo "Reformatted"
+    exit 0
     ;;
   *)
     echo "Canceled"
+    exit 1
    ;; 
 esac
-
