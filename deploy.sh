7252b987f86d92178ade37dfea5fdb64fc31e550
Henryk Paluch <hpaluch@seznam.cz>
2017-09-16 18:00:53 +0200

    Ignore list tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+cd `dirname $0`
+source _scripts/func.sh
+gen_app_yaml
 set -ex
 gcloud app deploy
+# please ignore "skipped"/"copied" messages before deployment
+# they have nothing common with Upload filter...
+#gcloud app deploy --verbosity=info
 gcloud app browse
 
