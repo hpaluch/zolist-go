eff2ff4599e4e96f28e69463d11c14441dd6b9b0
Henryk Paluch <hpaluch@seznam.cz>
2017-09-18 18:20:09 +0200

    Keep just last version

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,18 @@ source _scripts/func.sh
 gen_app_yaml
 set -e
 # check number of version (max 15)
-max_versions=15
+max_versions=2
 deployed_versions=$(gcloud app versions list | egrep -v '^SERVICE' | wc -l)
 
-if [ $deployed_versions -ge $max_versions ]
+dp_m1=$(( deployed_versions - 1 ))
+if [ "$dp_m1" -gt 1 ]
 then
-	last_version=$(gcloud app versions list |
-                       egrep -v '^SERVICE' | head -1 | awk '{print $2}')
-	[ -n "$last_version" ]
-	echo "Too many deployed versions $deployed_versions - deleteing last one: $last_version"
+	last_versions=$(gcloud app versions list |
+                       egrep -v '^SERVICE' | head -$dp_m1 | awk '{printf("%s ",$2)}')
+	[ -n "$last_versions" ]
+	echo "Too many deployed versions $deployed_versions - deleteing  $dp_m1"
 	set -x
-	gcloud app versions delete $last_version
+	gcloud app versions delete $last_versions
 	set +x
 fi
 set -x
